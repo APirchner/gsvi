f3b5b08e66ae1bfab881cb708a2904d44524aea3
AndreasP <andreas.pirchner@student.uibk.ac.at>
2019-10-09 10:39:12 +0200

    Worked on structure

--- gsvi/request_structures.py
+++ gsvi/request_structures.py
@@ -1,7 +1,9 @@
 import datetime
 import json
-from typing import List, Tuple
+from typing import Dict, List, Tuple, Union
 from abc import ABC, abstractmethod
+
+import pandas as pd
 
 from .google_connection import GoogleConnection
 
@@ -9,11 +11,9 @@ from .google_connection import GoogleConnection
 class BaseStructure(ABC):
     GRANULARITIES = ['DAY', 'HOUR']
 
-    def __init__(self, connection: GoogleConnection, keywords: List[str], start: datetime.datetime,
-                 end: datetime.datetime, geos: List[str], granularity='DAY'):
-        if len(keywords) != len(geos):
-            raise ValueError('Length of keyword list not equal to geos!')
-        elif not granularity.upper() in self.GRANULARITIES:
+    def __init__(self, connection: GoogleConnection, queries: Dict[str, Dict[str, str]], start: datetime.datetime,
+                 end: datetime.datetime, granularity='DAY'):
+        if not granularity.upper() in self.GRANULARITIES:
             raise ValueError('Invalid granularity!')
 
         # DAY: minimal time series has two entries
@@ -25,10 +25,9 @@ class BaseStructure(ABC):
         super().__init__()
 
         self.connection = connection
-        self.keywords = keywords
+        self.queries = queries
         self.start = start
         self.end = end
-        self.geos = geos
         self.granularity = granularity.upper()
         self.request_structure = None
         self.data = None
@@ -44,7 +43,7 @@ class BaseStructure(ABC):
         Splits the given interval into tuples of (lower, upper) suited
         for building requests in the requested granularity.
         """
-        interval_length = datetime.timedelta(days=30) if self.granularity == 'DAY' else datetime.timedelta(days=7)
+        interval_length = datetime.timedelta(days=90) if self.granularity == 'DAY' else datetime.timedelta(days=7)
         time_pointer = self.end
         intervals = []
         while time_pointer > self.start:
@@ -54,24 +53,28 @@ class BaseStructure(ABC):
             intervals.append((lower, upper))
         return intervals
 
+    def _get_max_request(self, queries) -> Tuple[datetime.datetime, datetime.datetime]:
+        # TODO: get the maximum interval for a single request (max 5 queries
+        pass
+
     @abstractmethod
-    def get_data(self):
+    def get_data(self, time_gap: int) -> Union[pd.Series, pd.DataFrame]:
         pass
 
 
 class TSUnivariate(BaseStructure):
-    def __init__(self, connection: GoogleConnection, keyword: str, start: datetime.datetime,
+    def __init__(self, connection: GoogleConnection, query: Dict[str, Dict[str, str]], start: datetime.datetime,
                  end: datetime.datetime, geo: str, granularity='DAY'):
-        super().__init__(connection, [keyword], start, end, [geo], granularity=granularity)
+        super().__init__(connection, query, start, end, granularity=granularity)
 
-    def get_data(self):
-        pass
+    def get_data(self, time_gap=10) -> pd.Series:
+        intervals = super()._build_intervals()
 
 
 class TSCrossSectional(BaseStructure):
-    def __init__(self, connection: GoogleConnection, keywords: List[str], start: datetime.datetime,
-                 end: datetime.datetime, geos: List[str], granularity='DAY'):
-        super().__init__(connection, keywords, start, end, geos, granularity=granularity)
+    def __init__(self, connection: GoogleConnection, query: Dict[str, Dict[str, str]], start: datetime.datetime,
+                 end: datetime.datetime, granularity='DAY'):
+        super().__init__(connection, query, start, end, granularity=granularity)
 
-    def get_data(self):
+    def get_data(self, time_gap=10) -> pd.DataFrame:
         pass
