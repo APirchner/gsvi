398aa1f549fd7935be0933b2b8cee9f3a91be999
AndreasP <andreas.pirchner@student.uibk.ac.at>
2019-10-17 12:26:37 +0200

    refactored request structure and added multivariate functionality

--- gsvi/request_structures.py
+++ gsvi/request_structures.py
@@ -4,40 +4,84 @@ import json
 import math
 import random
 from typing import Dict, List, Tuple, Union
-from abc import ABC, abstractmethod
 
 import pandas as pd
 
 from gsvi.google_connection import GoogleConnection
 
 
-class BaseStructure(ABC):
+class SVSeries(object):
+    MAX_FRGMNTS = 5
     GRANULARITIES = ['DAY', 'HOUR']
 
-    def __init__(self, connection: GoogleConnection, queries: List[Dict[str, str]], start: datetime.datetime,
-                 end: datetime.datetime, granularity='DAY', delay=10):
-        if not granularity.upper() in self.GRANULARITIES:
-            raise ValueError('Invalid granularity!')
-
-        # DAY: minimal time series has two entries
-        # HOUR: if interval is shorter than 3 days with HOUR, trends returns sub-hourly data
-        delta_min = datetime.timedelta(days=1) if granularity == 'DAY' else datetime.timedelta(days=3)
-        if start + delta_min > end:
-            raise ValueError('Invalid interval! DAY: min end is start + 2 days, HOUR min end is start + 2 days!')
-
-        if start < datetime.datetime(year=2004, month=1, day=1):
-            raise ValueError('Earliest date is 2004/01/01!')
-
-        super().__init__()
+    def __init__(self, connection: GoogleConnection, queries: List[Dict[str, str]],
+                 range: Tuple[datetime.datetime, datetime.datetime], granularity='DAY', delay=10):
 
         self.connection = connection
-        self.queries = queries
-        self.start = start
-        self.end = end
-        self.granularity = granularity.upper()
-        self.request_structure = None
-        self.data = None
         self.delay = delay
+        self.is_consistent = False
+        self._data = None
+        self._queries = queries
+        self._range = range
+        self._granularity = granularity.upper()
+        self._request_structure = None
+
+    @property
+    def queries(self):
+        return self._queries
+
+    @property
+    def range(self):
+        return self._range
+
+    @property
+    def granularity(self):
+        return self._granularity
+
+    @property
+    def request_structure(self):
+        return self._request_structure
+
+    @queries.setter
+    def queries(self, queries: List[Dict[str, str]]):
+        self.is_consistent = False
+        self._queries = queries
+
+    @range.setter
+    def range(self, range: Tuple[datetime.datetime, datetime.datetime]):
+        # DAY: minimal time series has two entries
+        # HOUR: if interval is shorter than 3 days with HOUR, trends returns sub-hourly data
+        delta_min = datetime.timedelta(days=1) if self.granularity == 'DAY' else datetime.timedelta(days=3)
+        if self.range[0] + delta_min > self.range[1]:
+            raise ValueError('Invalid interval! DAY: min end is start + 2 days, HOUR min end is start + 2 days!')
+
+        if self.range[0] < datetime.datetime(year=2004, month=1, day=1):
+            raise ValueError('Earliest date is 2004/01/01!')
+        self.is_consistent = False
+        self._range = range
+
+
+    @granularity.setter
+    def granularity(self, granularity: str):
+        if not granularity.upper() in self.GRANULARITIES:
+            raise ValueError('Invalid granularity!')
+        self.is_consistent = False
+        self._granularity = granularity
+
+    @request_structure.setter
+    def request_structure(self, request_structure):
+        self.is_consistent = False
+        self._request_structure = request_structure
+
+    @classmethod
+    def univariate(cls, connection: GoogleConnection, query: Dict[str, str], start: datetime.datetime,
+                   end: datetime.datetime, granularity='DAY', delay=10):
+        return cls(connection, [query], (start, end), granularity, delay)
+
+    @classmethod
+    def multivariate(cls, connection: GoogleConnection, queries: List[Dict[str, str]], start: datetime.datetime,
+                     end: datetime.datetime, granularity='DAY', delay=10):
+        return cls(connection, queries, (start, end), granularity, delay)
 
     def __repr__(self):
         return self.request_structure
@@ -52,9 +96,9 @@ class BaseStructure(ABC):
         """
         interval_length = datetime.timedelta(days=90) if self.granularity == 'DAY' else datetime.timedelta(days=7)
         offset = datetime.timedelta(days=1) if self.granularity == 'DAY' else datetime.timedelta(hours=1)
-        time_pointer = self.end
+        time_pointer = self.range[1]
         intervals = []
-        while time_pointer > self.start:
+        while time_pointer > self.range[0]:
             upper = time_pointer
             time_pointer -= interval_length
             lower = time_pointer + offset
@@ -62,77 +106,70 @@ class BaseStructure(ABC):
         return intervals
 
     def _get_max_request(self, queries) -> Dict[str, Union[str, Tuple[datetime.datetime, datetime.datetime]]]:
+        """ Finds the maximum search volume query out of a set of queries """
         response = self.connection.get_timeseries(queries, self.granularity)
         for i, query in enumerate(queries):
             if response[i].max() == 100:
                 return query
         raise ValueError('Response to queries {0} contains no max of 100'.format(queries))
 
-    @abstractmethod
-    def get_data(self) -> Union[pd.Series, pd.DataFrame]:
-        pass
-
-
-class TSUnivariate(BaseStructure):
-    def __init__(self, connection: GoogleConnection, query: Dict[str, str], start: datetime.datetime,
-                 end: datetime.datetime, granularity='DAY', delay=10):
-        super().__init__(connection, [query], start, end, granularity, delay)
-
-    def get_data(self) -> pd.Series:
+    def get_data(self) -> Union[pd.DataFrame, pd.Series]:
         '''
-        Builds the request structure for the query and makes the requests to
+        Builds the request structure for the queries and makes the requests to
         Google Trends such that the resulting time series values are between 0 and 100.
         Returns:
-        The normalized time series as pd.Series
+        The normalized time series as pd.Series (univariate) or pd.Dataframe (multivariate)
         '''
-        # PHASE 1 - get global maximum
-        requests = list()
-        requests.append([{**self.queries[0], **{'range': interval}} for interval in super()._build_intervals()])
-        depth = math.ceil(math.log(math.ceil(len(requests[0]) / 5), 5)) + 1
-        for i in range(0, depth):
-            layer = []
-            for j in range(0, len(requests[i]), 5):
-                layer.append(super()._get_max_request(requests[i][j:j + 5]))
-                # random delay between requests -> delay +/- 25%
-                time.sleep(self.delay + random.uniform(-self.delay * 0.25, self.delay * 0.25))
-            requests.append(layer)
-        # PHASE 2 - normalize over maximum
-        response = []
-        for i in range(0, len(requests[0]), 4):
-            response.extend(self.connection.get_timeseries(
-                queries=requests[0][i:i + 4] + requests[-1], granularity=self.granularity
-            )[:-1])
-        ts = pd.concat(response, verify_integrity=True).sort_index()
-        self.data = ts
-        self.request_structure = {'layer_{0}'.format(i): requests[i] for i in range(len(requests))}
-        return ts
 
+        if self._data is not None:
+            # return cached data to avoid getting the data again
+            return self._data
 
-class TSCrossSectional(BaseStructure):
+        # Preprocessing - flatten queries into the base layer of the request pyramid
+        intervals = self._build_intervals()
+        requests = [[]]
+        for query in self.queries:
+            requests[0].extend([{**query, **{'range': interval}} for interval in intervals])
 
-    def __init__(self, connection: GoogleConnection, query: List[Dict[str, str]], start: datetime.datetime,
-                 end: datetime.datetime, granularity='DAY', delay=10):
-        super().__init__(connection, query, start, end, granularity=granularity, delay=delay)
-
-    def get_data(self) -> pd.DataFrame:
-        # TODO: think about request structure for cross-sectional
-        for i in range(len(self.queries)):
-            requests = list()
-            requests.append([{**self.queries[i], **{'range': interval}} for interval in super()._build_intervals()])
-            depth = math.ceil(math.log(math.ceil(len(requests[0]) / 5), 5)) + 1
-            for j in range(0, depth):
+        # shortcut if query range fits into a single request (max 5 fragments per query)
+        if len(requests[0]) <= self.MAX_FRGMNTS:
+            response_flat = self.connection.get_timeseries(queries=requests[0], granularity=self.granularity)
+        else:
+            # Phase 1 - get global maximum
+            depth = math.ceil(math.log(math.ceil(len(requests[0]) / self.MAX_FRGMNTS), self.MAX_FRGMNTS)) + 1
+            for i in range(0, depth):
                 layer = []
-                for l in range(0, len(requests[j]), 5):
-                    layer.append(super()._get_max_request(requests[j][l:l + 5]))
+                for j in range(0, len(requests[i]), self.MAX_FRGMNTS):
+                    layer.append(self._get_max_request(requests[i][j:j + self.MAX_FRGMNTS]))
                     # random delay between requests -> delay +/- 25%
-                    time.sleep(self.delay + random.uniform(-self.delay * 0.25, self.delay * 0.25))
+                    time.sleep(self.delay + random.uniform(self.delay * -0.25, self.delay * 0.25))
                 requests.append(layer)
+            # Phase 2 - normalize over maximum
+            response_flat = []
+            for i in range(0, len(requests[0]), self.MAX_FRGMNTS - 1):
+                response_flat.extend(self.connection.get_timeseries(
+                    queries=requests[0][i:i + self.MAX_FRGMNTS - 1] + requests[-1], granularity=self.granularity
+                )[:-1])
+        # Postprocessing - unflatten normalized responses back into queries
+        response_stacked = []
+        for i in range(0, len(response_flat), len(intervals)):
+            series = pd.concat(response_flat[i:i + len(intervals)], verify_integrity=True).sort_index()
+            series.name = requests[0][i + len(intervals) - 1]['key']
+            response_stacked.append(series)
+        self._request_structure = {'layer_{0}'.format(i): requests[i] for i in range(len(requests))}
+        self.is_consistent = True
+        if len(response_stacked) == 1:
+            self._data = response_stacked[0].loc[self.range[0]:]
+        else:
+            self._data = pd.DataFrame(response_stacked).T.loc[self.range[0]:]
+        return self._data
 
 
 if __name__ == '__main__':
     gc = GoogleConnection(timeout=10)
-    start = datetime.datetime(year=2019, month=5, day=1)
-    end = datetime.datetime(year=2019, month=8, day=30)
-    ts = TSUnivariate(gc, {'key': 'apple', 'geo': 'US'}, start, end, 'DAY')
+    start = datetime.datetime(year=2017, month=1, day=1)
+    end = datetime.datetime(year=2019, month=9, day=30)
+    ts = SVSeries.multivariate(gc, [{'key': 'apple', 'geo': 'US'}, {'key': 'microsoft', 'geo': 'US'}],
+                             start, end, 'DAY')
     res = ts.get_data()
     print(ts)
