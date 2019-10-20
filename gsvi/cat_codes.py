0bfdf01773ff0a14a620b5730f7cb63f4ccfe902
AndreasP <andreas.pirchner@student.uibk.ac.at>
2019-10-20 05:54:24 +0200

    added category codes

---
+++ gsvi/cat_codes.py
@@ -0,0 +1,25 @@
+""" Holds Google Trends category codes. """
+import enum
+
+
+class CategoryCodes(enum.IntEnum):
+    """ Holds Google Trends category codes. """
+    NONE = 0
+    ARTS_ENTERTAINMENT = 3
+    AUTOS_VEHICLES = 47
+    BEAUTY_FITNESS = 44
+    BOOKS_LITERATURE = 22
+    BUSINESS_INDUSTRIAL = 12
+    COMPUTERS_ELECTRONICS = 5
+    FINANCE = 7
+    FOOD_DRINK = 71
+    GAMES = 8
+    HEALTH = 45
+    INTERNET_TELECOM = 13
+    JOBS_EDUCATION = 958
+    LAW_GOVERNMENT = 19
+    NEWS = 16
+    SCIENCE = 174
+    SHOPPING = 18
+    SPORTS = 20
+    TRAVEL = 67
